7f717759d558143e30f6152b6a8af71f52058827
rejatak <rajeshwarrn98@gmail.com>
2023-08-22 23:46:54 +0530

    day 3 changes

--- src/win32_handmade.cpp
+++ src/win32_handmade.cpp
@@ -1,5 +1,17 @@
 #include <windows.h>
 
+#define internal static
+#define local_persist static
+#define global_variable static
+
+// TODO: global variable for now
+global_variable bool running;
+
+global_variable BITMAPINFO bitmap_info;
+global_variable void *bitmap_memory;
+global_variable HBITMAP bitmap_handle;
+global_variable HDC bitmap_device_context;
+
 int DisplayResourceNAMessageBox()
 {
     int msgboxID = MessageBoxA(
@@ -26,6 +38,52 @@ int DisplayResourceNAMessageBox()
 }
 
 
+internal void win32_resize_DIB_section(
+    int width, int height
+) {
+    // TODO: Bulletproof this.
+    // Maybe don't free first, free after, then free first if that fails
+    
+    // TODO: Free our DIBSection
+    if (bitmap_handle) {
+        DeleteObject(bitmap_handle);
+    } 
+
+    if (!bitmap_device_context) {
+        // TODO: Should we recreate these under certain special circumstances
+        bitmap_device_context = CreateCompatibleDC(0);
+    }
+
+
+    BITMAPINFO bitmap_info;
+    bitmap_info.bmiHeader.biSize = sizeof(bitmap_info.bmiHeader);
+    bitmap_info.bmiHeader.biWidth = width;
+    bitmap_info.bmiHeader.biHeight = height;
+    bitmap_info.bmiHeader.biPlanes = 1;
+    bitmap_info.bmiHeader.biBitCount = 32;
+    bitmap_info.bmiHeader.biCompression = BI_RGB;
+    
+
+    void *bitmap_memory;
+    bitmap_handle = CreateDIBSection(
+        bitmap_device_context, &bitmap_info,
+        DIB_RGB_COLORS, &bitmap_memory,
+        0, 0);
+}
+
+internal void win32_update_window(
+    HDC device_context, int x, int y, int width, int height
+) {
+    StretchDIBits( device_context,
+        x, y, width, height,
+        x, y, width, height,
+        bitmap_memory,
+        &bitmap_info,
+        DIB_RGB_COLORS,
+        SRCCOPY
+    );
+}
+
 LRESULT CALLBACK mainwindow_callback(
     HWND    hWnd,
     UINT    Msg,
@@ -36,14 +94,20 @@ LRESULT CALLBACK mainwindow_callback(
     switch(Msg) {
         case WM_SIZE:
         {
-            OutputDebugStringA("WM_SIZE");
+            RECT client_rect;
+            GetClientRect(hWnd, &client_rect);
+            LONG height = client_rect.bottom - client_rect.top;
+            LONG width = client_rect.right - client_rect.left;
+            win32_resize_DIB_section(width, height);
         } break;
         case WM_DESTROY:
         {
+            running = false;
             OutputDebugStringA("WM_DESTROY");
         } break;
         case WM_CLOSE:
         {
+            running = false;
             OutputDebugStringA("WM_CLOSE");
         } break;
         case WM_ACTIVATEAPP:
@@ -60,8 +124,9 @@ LRESULT CALLBACK mainwindow_callback(
             LONG y = window_rect.top;
             LONG height = window_rect.bottom - window_rect.top;
             LONG width = window_rect.right - window_rect.left;
-            PatBlt(device_context, x, y, height, width, WHITENESS);
-            
+
+            win32_update_window(device_context, x, y, width, height);
+
             EndPaint(hWnd, &paint);
         } break;
         default:
@@ -113,8 +178,9 @@ int WINAPI WinMain(
         );
 
         if (main_window_handle) {
+            running = true;
             // get messages
-            while (TRUE) {
+            while (running) {
                 MSG messages;
                 BOOL message_result = GetMessage(&messages, 0, 0, 0);
                 if (message_result > 0) {
