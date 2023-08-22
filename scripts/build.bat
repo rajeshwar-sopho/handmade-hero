7f22aeeb7cb5edd8d00f7acf440ca4fbe949beb3
rejatak <rajeshwarrn98@gmail.com>
2023-08-23 01:36:31 +0530

    day 4 changes

--- scripts/build.bat
+++ scripts/build.bat
@@ -3,7 +3,8 @@
 mkdir ..\build
 pushd ..\build
 
-cl -Zi ..\src\win32_handmade.cpp User32.lib Gdi32.lib
+# -FAsc - jumps to the line with error
+cl -FAsc -Zi ..\src\win32_handmade.cpp User32.lib Gdi32.lib
 
 popd
  
--- src/win32_handmade.cpp
+++ src/win32_handmade.cpp
@@ -1,82 +1,85 @@
 #include <windows.h>
+#include <stdint.h>
 
 #define internal static
 #define local_persist static
 #define global_variable static
+
+#define BYTES_PER_PIXEL 4
+
+// typedef uint8_t uint8;
 
 // TODO: global variable for now
 global_variable bool running;
 
 global_variable BITMAPINFO bitmap_info;
 global_variable void *bitmap_memory;
-global_variable HBITMAP bitmap_handle;
-global_variable HDC bitmap_device_context;
+global_variable int bitmap_width;
+global_variable int bitmap_height;
 
-int DisplayResourceNAMessageBox()
-{
-    int msgboxID = MessageBoxA(
-        NULL,
-        "Resource not available\nDo you want to try again?",
-        "Account Details",
-        MB_ICONWARNING | MB_CANCELTRYCONTINUE | MB_DEFBUTTON2
-    );
+internal void render_wierd_gradient(int x_offset, int y_offset) {
+    int pitch = bitmap_width * BYTES_PER_PIXEL;
+    uint8_t *row = (uint8_t*) bitmap_memory;
+    for (int y=0; y < bitmap_height; ++y) {
+        // uint32_t *pixel = (uint32_t*) row;
+        uint32_t *pixel = (uint32_t*) row;
+        
+        for (int x=0; x < bitmap_width; ++x) {
+            /*
+                                  0  1  2  3
+                Pixel in memory: 00 00 00 00
+                                 BB GG RR xx
+                LITTLE ENDIAN ARCHITECTURE: lower byte is placed first
 
-    switch (msgboxID)
-    {
-    case IDCANCEL:
-        // TODO: add code
-        break;
-    case IDTRYAGAIN:
-        // TODO: add code
-        break;
-    case IDCONTINUE:
-        // TODO: add code
-        break;
-    }
+                0x xxBBGGRR --> 
+            */
 
-    return msgboxID;
+           uint8_t blue = x - x_offset;
+           uint8_t green = y - y_offset;
+           uint8_t red = 0;
+            
+            *pixel++ = ((red << 16) | (green << 8) | blue);
+        }
+
+        row += pitch;
+    } 
 }
 
 
-internal void win32_resize_DIB_section(
-    int width, int height
-) {
+internal void win32_resize_DIB_section(int width, int height) {
     // TODO: Bulletproof this.
     // Maybe don't free first, free after, then free first if that fails
-    
-    // TODO: Free our DIBSection
-    if (bitmap_handle) {
-        DeleteObject(bitmap_handle);
-    } 
 
-    if (!bitmap_device_context) {
-        // TODO: Should we recreate these under certain special circumstances
-        bitmap_device_context = CreateCompatibleDC(0);
+    if (bitmap_memory) {
+        VirtualFree(bitmap_memory, 0, MEM_RELEASE);
     }
 
+    bitmap_height = height;
+    bitmap_width = width;
 
-    BITMAPINFO bitmap_info;
     bitmap_info.bmiHeader.biSize = sizeof(bitmap_info.bmiHeader);
-    bitmap_info.bmiHeader.biWidth = width;
-    bitmap_info.bmiHeader.biHeight = height;
+    bitmap_info.bmiHeader.biWidth = bitmap_width;
+    bitmap_info.bmiHeader.biHeight = -bitmap_height; // this will make windows draw from top to down se the biheight on msdn
     bitmap_info.bmiHeader.biPlanes = 1;
     bitmap_info.bmiHeader.biBitCount = 32;
     bitmap_info.bmiHeader.biCompression = BI_RGB;
     
-
-    void *bitmap_memory;
-    bitmap_handle = CreateDIBSection(
-        bitmap_device_context, &bitmap_info,
-        DIB_RGB_COLORS, &bitmap_memory,
-        0, 0);
+    int bitmap_memory_size = BYTES_PER_PIXEL * bitmap_width * bitmap_height;
+    bitmap_memory = VirtualAlloc(0, bitmap_memory_size, MEM_COMMIT, PAGE_READWRITE);
 }
 
-internal void win32_update_window(
-    HDC device_context, int x, int y, int width, int height
-) {
+internal void win32_update_window(HDC device_context, RECT *client_rect) {
+    int window_width = client_rect->right - client_rect->left;
+    int window_height = client_rect->bottom - client_rect->top;
+
+
     StretchDIBits( device_context,
+        /*
         x, y, width, height,
         x, y, width, height,
+        */
+        0, 0, bitmap_width, bitmap_height,
+        0, 0, window_width, window_height,
         bitmap_memory,
         &bitmap_info,
         DIB_RGB_COLORS,
@@ -84,12 +87,7 @@ internal void win32_update_window(
     );
 }
 
-LRESULT CALLBACK mainwindow_callback(
-    HWND    hWnd,
-    UINT    Msg,
-    WPARAM  wParam,
-    LPARAM  lParam
-) {
+LRESULT CALLBACK mainwindow_callback(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam) {
     LRESULT result = 0;
     switch(Msg) {
         case WM_SIZE:
@@ -119,13 +117,10 @@ LRESULT CALLBACK mainwindow_callback(
             PAINTSTRUCT paint;
             HDC device_context = BeginPaint(hWnd, &paint);
 
-            RECT window_rect = paint.rcPaint;
-            LONG x = window_rect.left;
-            LONG y = window_rect.top;
-            LONG height = window_rect.bottom - window_rect.top;
-            LONG width = window_rect.right - window_rect.left;
+            RECT client_rect;
+            GetClientRect(hWnd, &client_rect);
 
-            win32_update_window(device_context, x, y, width, height);
+            win32_update_window(device_context, &client_rect);
 
             EndPaint(hWnd, &paint);
         } break;
@@ -179,16 +174,36 @@ int WINAPI WinMain(
 
         if (main_window_handle) {
             running = true;
-            // get messages
+            int x_offset, y_offset;
+            x_offset = y_offset = 0;
+            // get message
             while (running) {
-                MSG messages;
-                BOOL message_result = GetMessage(&messages, 0, 0, 0);
-                if (message_result > 0) {
-                    TranslateMessage(&messages);
-                    DispatchMessage(&messages);
-                } else {
-                    break;
+                MSG message;
+                // peek message loop when there is no message it will allow us to run instead of blocking
+                while(PeekMessageA(&message, 0, 0, 0, PM_REMOVE)) {
+                    if (message.message == WM_QUIT) {
+                        running = false;
+                    }
+
+                    TranslateMessage(&message);
+                    DispatchMessage(&message);
                 }
+
+                render_wierd_gradient(x_offset, y_offset);
+
+                ++x_offset;
+                y_offset -= 2;
+                
+
+                HDC device_context = GetDC(main_window_handle);
+                RECT client_rect;
+                GetClientRect(main_window_handle, &client_rect);
+
+                int window_width = client_rect.right - client_rect.left;
+                int window_height = client_rect.bottom - client_rect.top;
+
+                win32_update_window(device_context, &client_rect);
+                ReleaseDC(main_window_handle, device_context);
             }
         } else {
             // TODO: logging
